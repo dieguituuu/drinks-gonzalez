50af1997c719902062764fcd737baa4ce0d885ce
Diego Gonzalez <diegocoderhouse@gmail.com>
2022-09-20 15:13:53 -0300

    pagina a medias

---
+++ index.js
@@ -0,0 +1,24 @@
+// const edad = parseInt(prompt('¿Cuantos años tenes?'))
+
+// edad >= 18 ? alert('¡Bienvenido/a a Mystic Drinks!') : alert('Sos menor de edad, no podes comprar alcohol.')
+
+const productos = [
+    {id: 1, nombre: 'Jägermeister', precio: '$2,200', img: 'jager.jpg'},
+    {id: 2, nombre: 'Smirnoff', precio: '$1.400', img: 'smirnoff.jpg'},
+    {id: 3, nombre: 'Fernet Branca', precio: '$1.100', img: 'fernet.jpg'},
+    {id: 4, nombre: 'Absolut', precio: '$1,650', img: 'absolut.jpg'},
+    {id: 5, nombre: 'Jack Daniels', precio: '$3,400', img: 'jack-daniels.jpg'},
+    {id: 6, nombre: 'Speed', precio: '$325', img: 'speed.jpg'},
+    {id: 7, nombre: 'Red Bull', precio: '$350', img: 'red-bull.jpg'},
+    {id: 8, nombre: 'Coca-Cola', precio: '$280', img: 'coca-cola.jpg'},
+    {id: 9, nombre: 'Baggio', precio: '$190', img: 'baggio.jpg'},
+    {id: 10, nombre: 'Termidor', precio: '$240', img: 'baggio.jpg'},
+    {id: 11, nombre: 'Manaos', precio: '$160', img: 'manaos.jpg'}    
+]
+
+const cards = document.querySelector('.main__content')
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
