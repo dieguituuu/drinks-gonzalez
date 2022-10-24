323a1bedd006c3715e3a444294550536d16c7688
Diego Gonzalez <diegocoderhouse@gmail.com>
2022-10-24 00:29:54 -0300

    pagina casi terminada, faltan detalles

--- js/index.js
+++ js/index.js
@@ -1,34 +1,37 @@
-const edad = parseInt(prompt('¿Cuantos años tenes?'))
+// Se pregunta la edad para corroborar que sea mayor de edad. En caso de que no lo sea, no se le permite entrar en la página.
+// const edad = parseInt(prompt('¿Cuantos años tenes?'))
 
+// if(edad >=18) {
+//     Swal.fire(
+//         '¡Bienvenido/a a Mystic Drinks!',
+//         'Tenes edad para comprar alcohol.',
+//         'success'
+//       )
+//   } else {
+//     Swal.fire(
+//         ':(',
+//         'Sos menor de edad.',
+//         'error'
+//       )
+//     document.getElementById('blanco').style.display = "none";
+//   }
+
+// Constantes necesarias para el funcionamiento del carrito.
 const carritoDiv = document.getElementById('carrito')
 const contenedorCarrito = document.getElementById('carrito-contenedor')
 const botonCarrito = document.getElementById('boton-carrito')
+const botonVaciar = document.getElementById('vaciar-carrito')
+const contadorCarrito = document.getElementById('contadorCarrito')
+const precioTotal = document.getElementById('precioTotal')
 
-const carrito = []
+let carrito = JSON.parse(localStorage.getItem('carrito')) || [];
 
-if(edad >=18) {
-    Swal.fire(
-        '¡Bienvenido/a a Mystic Drinks!',
-        'Tenes edad para comprar alcohol.',
-        'success'
-      )
-  } else {
-    Swal.fire(
-        ':(',
-        'Sos menor de edad.',
-        'error'
-      )
-    document.getElementById('blanco').style.display = "none";
-  }
-
-const producto = fetch('./js/productos.json')
-.then(response=>response.json())
-.then(info => {
+function crearProductos(arr) {
     const contenedorProductos = document.getElementById('prod-content')
-    info.forEach((producto) => {
+    arr.forEach((producto) => {
         const div = document.createElement('div')
-    div.classList.add('producto')
-    div.innerHTML =  `
+        div.classList.add('producto')
+        div.innerHTML =  `
     <div class='prod-cards'>
     <img src='./assets/${producto.img}' alt='' class='prod-img'>
     <h3 class='prod-nombre'>${producto.nombre}</h3>
@@ -37,48 +40,119 @@ const producto = fetch('./js/productos.json')
     </div>
     `
     contenedorProductos.appendChild(div)
-    const boton = document.getElementById(`agregar${producto.id}`)
-    boton.addEventListener('click', () => {
-        agregarAlCarrito(producto.id)
-    })  
 })
-})
- 
-const agregarAlCarrito = (prodId) => {
-    const item = producto.find((prod) => prod.id === prodId)
-    const existe = carrito.some(prod=>prod.id === producto.id)
-    if(existe === false) {
-        producto.cantidad = 1;
-        carrito.push(producto)
-    } else {
-        const encontrarProd = carrito.find(prod=> prod.id ===producto.id);
-        encontrarProd.cantidad++;
+}
+
+
+
+
+// Se guardan los productos en localStorage.
+document.addEventListener('DOMContentLoaded', () => {
+    if (localStorage.getItem('carrito')){
+        carrito = JSON.parse(localStorage.getItem('carrito'))
+        // actualizarCarrito()
     }
-    carrito.push(item)
+})
+
+// Funcionalidad al boton "vaciar carrito".
+botonVaciar.addEventListener('click', () => {
+    carrito = []
+    actualizarCarrito()
+})
+
+function agregarFuncionBoton(arr) {
+    arr.forEach(producto => {  
+    document.querySelector(`#agregar${producto.id}`).addEventListener('click', () => {
+    agregarAlCarrito(producto)})
+})
+}  
+
+function agregarAlCarrito(producto){
+        let existe = carrito.some(prod=>prod.id === producto.id)
+        if(existe === false){
+          producto.cantidad = 1;
+          carrito.push(producto)
+        }
+        else{
+          let prodFind = carrito.find(prod=> prod.id === producto.id);
+          prodFind.cantidad++;
+        }
+        actualizarCarrito()
+}        
+
+function actualizarCarrito()  {
+    carrito.forEach(producto=> {
+        let div = document.createElement('div')
+        div.innerHTML +=`
+        <div class='productoEnCarrito'>
+        <p>${producto.nombre}</p>
+        <p class= 'carrito-prod-precio'>${producto.precio}</p>
+        <p class= 'carrito-prod-cant'>${producto.cantidad}</p>
+        <button onclick='eliminarDelCarrito(${producto.id})' class='boton-eliminar'><i class='fas fa-trash-alt'></i></button>
+        </div>
+        `
+        contenedorCarrito.appendChild(div)
+
+    })
+    localStorage.setItem('carrito', JSON.stringify(carrito))
+    contadorCarrito.innerText = carrito.lenght
+    precioTotal.innerText = carrito.reduce((acc, prod) => acc + prod.precio, 0)
+}
+
+// Función para eliminar productos del carrito.
+const eliminarDelCarrito = (prodId) => {
+    const item = carrito.find((prod) => prod.id === prodId)
+    const indice = carrito.indexOf(item)
+    carrito.splice(indice, 1)
     actualizarCarrito()
 }
 
-const actualizarCarrito = () => {
-    carritoDiv.innerHTML = '';
-    carrito.forEach(producto=> {
-        carritoDiv.innerHTML +=`
-        <div class= 'carrito-prod'>
-        <h3 class= 'carrito-prod-nombre'>${producto.nombre}</h3>
-        <p class= 'carrito-prod-cant'></p>
-        <p class= 'carrito-prod-precio'>${producto.precio}</p>
+// Función para actualizar el carrito.
 
-        `
-    }
-        )
-}
-
-function renderizarCarrito() {
+// Función para que se acumulen los productos iguales dentro del carrito.
+function renderizarCarrito(producto) {
     const existe = carrito.some(prod=>prod.id === producto.id)
     if(existe === false) {
-        producto.cantidad = 1;
+        producto.cant = 1;
         carrito.push(producto)
     } else {
         const encontrarProd = carrito.find(prod=> prod.id ===producto.id);
-        encontrarProd.cantidad++;
+        encontrarProd.cant++;
     }
 }
+
+// Constantes necesarias para el modal del carrito.
+const contenedorModal = document.getElementsByClassName('modal-contenedor')[0]
+const botonAbrir = document.getElementById('boton-carrito')
+const botonCerrar = document.getElementById('carritoCerrar')
+const modalCarrito = document.getElementsByClassName('modal-carrito')[0]
+
+
+// Evento para abrir el modal del carrito.
+botonAbrir.addEventListener('click', ()=>{
+    contenedorModal.classList.toggle('modal-active')
+})
+
+// Evento para cerrar el modal del carrito.
+botonCerrar.addEventListener('click', ()=>{
+    contenedorModal.classList.toggle('modal-active')
+})
+
+// Evento para poder cerrar el modal del carrito.
+contenedorModal.addEventListener('click', (event) =>{
+    contenedorModal.classList.toggle('modal-active')
+})
+
+// Evento para que no se cierre el modal del carrito automáticamente al vaciarlo.
+modalCarrito.addEventListener('click', (event) => {
+    event.stopPropagation() 
+})
+
+function productos() {
+    fetch('./js/productos.json')
+    .then(res=>res.json())
+    .then(info => { crearProductos(info)
+    agregarFuncionBoton(info)})
+}
+
+productos()
